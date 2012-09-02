fe1cae9a9ad272437217dd69be9dae61b61b20f8
jeremy <jeremy.gabriele@gmail.com>
2012-09-02 18:34:21 +0200

    Vanilla rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fruity::Application.config.secret_token = '48b294e5733a5f11fb09166b6f46b77eb915cc1b10433f0930d194f25349fce8dcdf2f3c49b53486c2fc97209065f26b8073b03fd2657dbbd54ece3e1bea9cec'
